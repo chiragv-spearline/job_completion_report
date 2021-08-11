3bd74b84fe067aacce5d3882293f541ca7f6f6a2
Chirag Vadodariya <chirag.vadodariya@spearline.com>
2021-08-11 17:35:25 +0530

    Added code for linktest and outbound conf test-types

--- data.py
+++ data.py
@@ -20,6 +20,8 @@ sip_trunk_test_type_id = 63
 conference_longcall_test_type_id = 11
 conference_with_tones_test_type_id = 15
 international_conference_test_type_id = 18
+outbound_conference_test_type_id = 23
+linktest_test_type_id = 24
 
 # Numbers for generating campaign job schedule report
 pstn_conf_qual_numbers = {
@@ -57,3 +59,5 @@ sip_trunk_testtype_report_headers = ['ID', 'Number', 'Campaign', 'Country', 'Num
 conference_longcall_testtype_report_headers = ['ID', 'Number', 'Campaign', 'Country', 'Number Type', 'Customer', 'Department', 'Location', 'Carrier', 'CLI', 'IVR Traversal', 'Call Start Time', 'Call End Time', 'State', 'Score', 'Recording', 'Phonegroup', 'Region', 'Moderator Passcode', 'Participant Passcode', 'Bridge']
 conference_with_tones_testtype_report_headers = ['ID', 'Number', 'Campaign', 'Country', 'Number Type', 'Customer', 'Department', 'Location', 'Carrier', 'CLI', 'IVR Traversal', 'Call Start Time', 'Call End Time', 'State', 'Score', 'Recording', 'Number', 'Country', 'Start Time', 'Connect Time', 'DDI End Time']
 international_conference_testtype_report_headers = ['ID', 'Number', 'Campaign', 'Country', 'Number Type', 'Customer', 'Department', 'Location', 'Carrier', 'CLI', 'IVR Traversal', 'Call Start Time', 'Call End Time', 'State', 'Score', 'Recording', 'Number', 'Country', 'Start Time', 'Connect Time', 'DDI End Time']
+outbound_conference_testtype_report_headers = []
+linktest_testtype_report_headers = ['ID', 'Number', 'Campaign', 'Country', 'Number Type', 'Customer', 'Department', 'Location', 'Carrier', 'CLI', 'IVR Traversal', 'Call Start Time', 'Call End Time', 'State', 'Score', 'Recording', 'Phonegroup', 'Region', 'Moderator Passcode', 'Participant Passcode', 'Bridge']
